985d7c01d3b837c3c665e737cb78bb05816891ea
malain <alain.metge@zenasoft.com>
2018-01-20 14:40:28 +0100

    update code

--- 05-Calling other services/api/myQueryHandler.ts
+++ 05-Calling other services/api/myQueryHandler.ts
@@ -2,17 +2,16 @@ import { QueryHandler, Query, DefaultQueryHandler } from "vulcain-corejs";
 import { Service1Proxy, Customer } from "./service110";
 
 @QueryHandler({ scope: '?' })
-export class MyActionHandler extends DefaultQueryHandler<Customer> {
+export class MyQueryHandler extends DefaultQueryHandler<Customer> {
     
     // Extends default actions with a new action
     // By default action name is the method name 
     // But you can force un name with the action annotation property
-    @Query({description: "Returns a list of customer full names", action: "fullnames", outputSchema: "string"})
+    @Query({description: "Returns a list of customer full names", action: "fullnames", outputType: "many", outputSchema: "string"})
     async getFullNames() {
         
         let proxy = new Service1Proxy(this.context);
         let res = await proxy.getAllCustomer();
-        res.value = res.value.map(c => c.firstName + " " + c.lastName);
-        return res;
+        return { ...res, value: res.value.map(c => c.firstName + " " + c.lastName) };
     }
 }
